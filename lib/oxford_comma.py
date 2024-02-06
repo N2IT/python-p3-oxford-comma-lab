f2d3e78a3226a9715b8586ac22e858abeae8e448
Tony Eder <tonyeder11@gmail.com>
2024-02-06 10:48:23 -0800

    passed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+
+    # if a single element return untouched
+    
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    # if more than two elements return el 1 AND el 2
+    elif len(items) >= 3:
+        last = items.pop(-1)
+        pen_ult = items.pop(-1)
+        new_list = [ pen_ult, last ]
+        new_new_list = ", and ".join(new_list)
+        items.append(new_new_list)
+        last_list = ", ".join(items)
+        return last_list
